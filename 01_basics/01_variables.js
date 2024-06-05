a1a7911a9fa4c0ca12253f27b3ab6d9d03837f4d
Manasvi Gupta <143724123+manasvi1684@users.noreply.github.com>
2024-06-05 06:09:13 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144535 //constant value
+let accountEmail="manasvi@gmail.com" //values can be changed ,therefore they are mutable
+let accountPassword="12345"
+accountCity="Bangalore"
+accountEmail="manu@gmail.com"
+accountPassword="6789"
+accountCity="jaipur"
+let accountState;// when no value is give to a variable js take it "undefined"
+// accountId=2 // NOT ALLOWED
+ console.log(accountId);
+  console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+Prefer not to use var becausze of issue in block scope and functional scope.
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // We are using node js not browser
+
+console.log(3+3) //code readability shd be
+let age=19
+let state;
+let isloggedin 
+
+//Datatypes
+// number => 2 to power 45
+// bigint
+// string => ""
+// boolean =>true/false
+// null =>standalone value
+// undefined =>
+// symbol
+// object
+console.log(typeof age)
+console.log(typeof null) //type-object
+console.log(typeof undefined) //type-undefined
