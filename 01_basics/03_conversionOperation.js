9a942f3be27fb651dc5984362e67b7f9ff1936bf
Manasvi Gupta <143724123+manasvi1684@users.noreply.github.com>
2024-06-05 06:49:04 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,32 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+// *********************************************OPERATIONS**********************************************************************************//
+let value=3
+let negValue= -value
+console.log(negValue)
+
+let str1="hello"
+let str2=" manu"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2+2); //if string first all values are treated as strings 
+console.log(1+2+"2"); //IF STRING last first conversion is done
+console.log(1+"2"+2);
+console.log(true);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+console.log(gameCounter);
+
+
