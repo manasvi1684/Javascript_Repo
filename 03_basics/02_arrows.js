8777afb529d43b27907a4c614f739e70c65b6cc5
Manasvi Gupta <143724123+manasvi1684@users.noreply.github.com>
2024-06-06 04:46:31 +0000

    arrows

---
+++ 03_basics/02_arrows.js
@@ -0,0 +1,25 @@
+// const user={
+//     username:"manasvi",
+//     price:999,
+     
+//     welcomeMessage: function(){
+//         console.log(`${this.username} welcome to website`)
+//         console.log(this)
+//     }
+
+// }
+//  user.welcomeMessage()
+//  user.username="sam"
+//  user.welcomeMessage()
+// console.log(this)
+
+// function chai(){
+//     let username="manasvi"
+//     console.log(this.username)
+// }
+// chai()  //UNDEFINED
+
+//ARRW FUNCTION
+const addTwo =(num1,num2) =>  num1+num2  //With paranthesis u need to add 'return' keyword whereass without paranthesis u dont need to add 'return' keyword
+
+console.log(addTwo(3,4))
